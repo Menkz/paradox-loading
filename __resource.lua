f8d91c0accae2edcc63642975428127c8d5b87ac
Menkz <iammenkz@gmail.com>
2019-05-05 12:17:12 +1000

    Added files to git

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+files {
+	'index.html',
+	'assets/css/bootstrap.min.css',
+	'assets/css/custom.css',
+	'assets/slick/slick.css',
+	'assets/slick/slick.min.js',
+	'assets/imgs/bg.png',
+	'assets/imgs/logo.png'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
